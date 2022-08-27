afe37e320fbc3386dfc18c5c6032923cde112541
limpbiskuit <gimlilegamer@gmail.com>
2022-08-27 13:10:43 +0200

    test

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -2,6 +2,6 @@ function fish_right_prompt
   set -l comment (set_color 444)
   set -l normal (set_color normal)
   set -l bold (set_color --bold)
-  
+
   echo -n -s $comment [ $bold (date +%H:%M:%S) $normal $comment ]
-end
+end 
